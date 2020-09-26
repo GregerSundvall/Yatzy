2f86e2c5975a6c30f41c17c7dbc8e10a648888a0
gregersundvall <gregersundvall@me.com>
2020-09-26 21:23:24 +0200

    Refactoring ScoreboardActivity.kt

--- app/src/main/java/com/example/yatzy/ScoreboardActivity.kt
+++ app/src/main/java/com/example/yatzy/ScoreboardActivity.kt
@@ -11,8 +11,6 @@ class ScoreboardActivity : AppCompatActivity() {
 
     val listOfPlayers = Players.listOfPlayers
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scoreboard)
@@ -20,86 +18,57 @@ class ScoreboardActivity : AppCompatActivity() {
         showScores()
     }
 
-        //writes out names and scores on screen
+        //Shows necessary fields and writes out info
     fun showScores(){
-        val p1TextView = findViewById<TextView>(R.id.name1TextView)
-        val s1TextView = findViewById<TextView>(R.id.score1TextView)
-        val p2TextView = findViewById<TextView>(R.id.name2TextView)
-        val s2TextView = findViewById<TextView>(R.id.score2TextView)
-        val p3TextView = findViewById<TextView>(R.id.name3TextView)
-        val s3TextView = findViewById<TextView>(R.id.score3TextView)
-        val p4TextView = findViewById<TextView>(R.id.name4TextView)
-        val s4TextView = findViewById<TextView>(R.id.score4TextView)
-        val p5TextView = findViewById<TextView>(R.id.name5TextView)
-        val s5TextView = findViewById<TextView>(R.id.score5TextView)
-        val p6TextView = findViewById<TextView>(R.id.name6TextView)
-        val s6TextView = findViewById<TextView>(R.id.score6TextView)
+        val p1NameTextView = findViewById<TextView>(R.id.name1TextView)
+        val p1ScoreTextView = findViewById<TextView>(R.id.score1TextView)
+        val p2NameTextView = findViewById<TextView>(R.id.name2TextView)
+        val p2ScoreTextView = findViewById<TextView>(R.id.score2TextView)
+        val p3NameTextView = findViewById<TextView>(R.id.name3TextView)
+        val p3ScoreTextView = findViewById<TextView>(R.id.score3TextView)
+        val p4NameTextView = findViewById<TextView>(R.id.name4TextView)
+        val p4ScoreTextView = findViewById<TextView>(R.id.score4TextView)
+        val p5NameTextView = findViewById<TextView>(R.id.name5TextView)
+        val p5ScoreTextView = findViewById<TextView>(R.id.score5TextView)
+        val p6NameTextView = findViewById<TextView>(R.id.name6TextView)
+        val p6ScoreTextView = findViewById<TextView>(R.id.score6TextView)
 
-        when(listOfPlayers.size){
-            2 -> {  p1TextView.text = listOfPlayers[0].name
-                    s1TextView.text = listOfPlayers[0].scoreSheet[17].points.toString()
-                    p2TextView.text = listOfPlayers[1].name
-                    s2TextView.text = listOfPlayers[1].scoreSheet[17].points.toString()
-                    p3TextView.visibility = View.INVISIBLE
-                    s3TextView.visibility = View.INVISIBLE
-                    p4TextView.visibility = View.INVISIBLE
-                    s4TextView.visibility = View.INVISIBLE
-                    p5TextView.visibility = View.INVISIBLE
-                    s5TextView.visibility = View.INVISIBLE
-                    p6TextView.visibility = View.INVISIBLE
-                    s6TextView.visibility = View.INVISIBLE
-            }
-            3 -> {  p1TextView.text = listOfPlayers[0].name
-                    s1TextView.text = listOfPlayers[0].scoreSheet[17].points.toString()
-                    p2TextView.text = listOfPlayers[1].name
-                    s2TextView.text = listOfPlayers[1].scoreSheet[17].points.toString()
-                    p3TextView.text = listOfPlayers[2].name
-                    s3TextView.text = listOfPlayers[2].scoreSheet[17].points.toString()
-                    p4TextView.visibility = View.INVISIBLE
-                    s4TextView.visibility = View.INVISIBLE
-                    p5TextView.visibility = View.INVISIBLE
-                    s5TextView.visibility = View.INVISIBLE
-                    p6TextView.visibility = View.INVISIBLE
-                    s6TextView.visibility = View.INVISIBLE
-            }
-            4 -> {  p1TextView.text = listOfPlayers[0].name
-                    s1TextView.text = listOfPlayers[0].scoreSheet[17].points.toString()
-                    p2TextView.text = listOfPlayers[1].name
-                    s2TextView.text = listOfPlayers[1].scoreSheet[17].points.toString()
-                    p3TextView.text = listOfPlayers[2].name
-                    s3TextView.text = listOfPlayers[2].scoreSheet[17].points.toString()
-                    p4TextView.text = listOfPlayers[3].name
-                    s4TextView.text = listOfPlayers[3].scoreSheet[17].points.toString()
-                    p5TextView.visibility = View.INVISIBLE
-                    s5TextView.visibility = View.INVISIBLE
-                    p6TextView.visibility = View.INVISIBLE
-                    s6TextView.visibility = View.INVISIBLE
-            }
-            5 -> { p1TextView.text = listOfPlayers[0].name
-                    s1TextView.text = listOfPlayers[0].scoreSheet[17].points.toString()
-                    p2TextView.text = listOfPlayers[1].name
-                    s2TextView.text = listOfPlayers[1].scoreSheet[17].points.toString()
-                    p3TextView.text = listOfPlayers[2].name
-                    s3TextView.text = listOfPlayers[2].scoreSheet[17].points.toString()
-                    p4TextView.text = listOfPlayers[3].name
-                    s4TextView.text = listOfPlayers[3].scoreSheet[17].points.toString()
-                    p5TextView.text = listOfPlayers[4].name
-                    s5TextView.text = listOfPlayers[4].scoreSheet[17].points.toString()
-                    p6TextView.visibility = View.INVISIBLE
-                    s6TextView.visibility = View.INVISIBLE
-            }
-            6 -> { p1TextView.text = listOfPlayers[0].name
-                    s1TextView.text = listOfPlayers[0].scoreSheet[17].points.toString()
-                    p2TextView.text = listOfPlayers[1].name
-                    s2TextView.text = listOfPlayers[1].scoreSheet[17].points.toString()
-                    p3TextView.text = listOfPlayers[2].name
-                    s3TextView.text = listOfPlayers[2].scoreSheet[17].points.toString()
-                    p4TextView.text = listOfPlayers[3].name
-                    s4TextView.text = listOfPlayers[3].scoreSheet[17].points.toString()
-                    p5TextView.text = listOfPlayers[4].name
-                    s5TextView.text = listOfPlayers[4].scoreSheet[17].points.toString()
-                    p6TextView.text = listOfPlayers[5].name
-                    s6TextView.text = listOfPlayers[5].scoreSheet[17].points.toString()
+        p1NameTextView.text = listOfPlayers[0].name
+        p1ScoreTextView.text = listOfPlayers[0].scoreSheet[17].points.toString()
+        p2NameTextView.text = listOfPlayers[1].name
+        p2ScoreTextView.text = listOfPlayers[1].scoreSheet[17].points.toString()
+        p3NameTextView.visibility = View.INVISIBLE
+        p3ScoreTextView.visibility = View.INVISIBLE
+        p4NameTextView.visibility = View.INVISIBLE
+        p4ScoreTextView.visibility = View.INVISIBLE
+        p5NameTextView.visibility = View.INVISIBLE
+        p5ScoreTextView.visibility = View.INVISIBLE
+        p6NameTextView.visibility = View.INVISIBLE
+        p6ScoreTextView.visibility = View.INVISIBLE
+
+
+        if(listOfPlayers.size >= 3){
+            p3NameTextView.text = listOfPlayers[2].name
+            p3ScoreTextView.text = listOfPlayers[2].scoreSheet[17].points.toString()
+            p3NameTextView.visibility = View.VISIBLE
+            p3ScoreTextView.visibility = View.VISIBLE
+            if(listOfPlayers.size >= 4){
+                p4NameTextView.visibility = View.VISIBLE
+                p4ScoreTextView.visibility = View.VISIBLE
+                p4NameTextView.text = listOfPlayers[3].name
+                p4ScoreTextView.text = listOfPlayers[3].scoreSheet[17].points.toString()
+                if(listOfPlayers.size >= 5){
+                    p5NameTextView.visibility = View.VISIBLE
+                    p5ScoreTextView.visibility = View.VISIBLE
+                    p5NameTextView.text = listOfPlayers[4].name
+                    p5ScoreTextView.text = listOfPlayers[4].scoreSheet[17].points.toString()
+                    if(listOfPlayers.size == 6){
+                        p6NameTextView.visibility = View.VISIBLE
+                        p6ScoreTextView.visibility = View.VISIBLE
+                        p6NameTextView.text = listOfPlayers[5].name
+                        p6ScoreTextView.text = listOfPlayers[5].scoreSheet[17].points.toString()
+                    }
+                }
             }
         }
     }
